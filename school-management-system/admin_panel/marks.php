775a1fa110ef18ed307c2297cae6641cd0639ee3
Samboblue <sumsambo8899@gmail.com>
2026-06-14 21:30:53 +0700

    yy jreh mes 2

--- school-management-system/admin_panel/marks.php
+++ school-management-system/admin_panel/marks.php
@@ -15,7 +15,7 @@
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Roll No.</th>
-                            <th scope="col">Name </th>
+                            <th scope="col">Name :</th>
                             <th scope="col">Obtained</th>
 
                         </tr>
