ccc5fdaaaff35059654a10e91f6f401836001afc
NY ENAT <soknyenat1@gmail.com>
2026-06-15 02:51:09 +0700

    testing

--- school-management-system/login.php
+++ school-management-system/login.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['uid'])) {
 
 <head>
   <meta charset="UTF-8">
-  <title>School Management System</title>
+  <title>School </title>
   <!-- Fontawesome CDN Link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
